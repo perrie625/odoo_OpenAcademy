de5f2925b3d97e9570c27019fa4e12d64e5883dc
Perry <q_perry@163.com>
2015-03-12 16:02:19 +0800

    add partner.py and partner.xml

--- __openerp__.py
+++ __openerp__.py
@@ -28,6 +28,7 @@
         # 'security/ir.model.access.csv',
         'templates.xml',
         'view/openacademy.xml',
+        'view/partner.xml',
         ],
     # only loaded in demonstration mode
     'demo': [
