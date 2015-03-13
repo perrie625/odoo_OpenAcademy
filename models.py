091254b37c26d7a0f07742548d4e0ce9bb735c3d
Perry <q_perry@163.com>
2015-03-13 17:07:49 +0800

    List color & calendar view in session

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+from datetime import timedelta
 from openerp import models, fields, api, exceptions
 
 class Course(models.Model):
@@ -55,6 +55,8 @@ class Session(models.Model):
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
 
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+    end_date = fields.Date(string='End Date', store=True,
+                                  compute='_get_end_date', inverse='_set_end_date')
 
     @api.one
     @api.depends('seats', 'attendee_ids')
@@ -82,8 +84,32 @@ class Session(models.Model):
             }
 
     @api.one
+    @api.depends('start_date', 'duration')
+    def _get_end_date(self):
+        if not (self.start_date and self.duration):
+            self.end_date = self.start_date
+            return
+
+        # Add duration to start_date, but: Monday + 5days = Saturday, so
+        # subtract one second to get on Friday instead
+        start = fields.Datetime.from_string(self.start_date)
+        duration = timedelta(days=self.duration, seconds=-1)
+        self.end_date = start + duration
+
+    @api.one
+    def _set_end_date(self):
+        if not (self.start_date and self.end_date):
+            return
+
+        # compute the different between dates, but: Fraday - Monday = 4 days,
+        # so add one day to get 5 days instead
+        start_date = fields.Datetime.from_string(self.start_date)
+        end_date = fields.Datetime.from_string(self.end_date)
+        self.duration = (end_date - start_date).days + 1
+
+    @api.one
     @api.constrains('instructor_id', 'attendee_ids')
-    def _check_instructor_not_in_attendees(selfs):
+    def _check_instructor_not_in_attendees(self):
         if self.instructor_id and self.instructor_id in self.attendee_ids:
             raise exceptions.ValidationError("A session's instructor can't be an attendeee")
             # class openacademy(models.Model):
