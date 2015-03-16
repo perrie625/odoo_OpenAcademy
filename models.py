50ba4ef650785e8f7fd1640137e6e17e3c909e30
Perry <q_perry@163.com>
2015-03-16 10:45:15 +0800

    Almost a workflow

--- models.py
+++ models.py
@@ -64,6 +64,24 @@ class Session(models.Model):
     attendees_count = fields.Integer(
         string="Attendees count", compute='_get_attendees_count', store=True)
 
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('confirmed', 'Confirmed'),
+        ('done', 'Done'),
+    ], default='draft')
+
+    @api.one
+    def action_draft(self):
+        self.state = 'draft'
+
+    @api.one
+    def action_confirm(self):
+        self.state = 'confirmed'
+
+    @api.one
+    def action_done(self):
+        self.state = 'done'
+
     @api.one
     @api.depends('seats', 'attendee_ids')
     def _taken_seats(self):
